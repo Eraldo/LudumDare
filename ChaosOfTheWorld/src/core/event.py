1ff6af3d7f37d34c1627ec310cd849faa9de0482
Eraldo Helal <eraldo@eraldo.org>
2012-12-18 19:25:55 +0100

    added blood exhaustion every 4th day and added texture for known shelters

--- ChaosOfTheWorld/src/core/event.py
+++ ChaosOfTheWorld/src/core/event.py
@@ -5,6 +5,7 @@ Created on 15.12.2012
 '''
 import gameState
 from graphic import *
+from random import randint
 
 class Event(object):
     def __init__(self, game):
@@ -24,26 +25,30 @@ class DayBreakEvent(Event):
         super(DayBreakEvent, self).__init__(game)
         self.name = "daybreak"
         self.texture = Texture("welcome.png")
+        self.texture_consumed = Texture("welcome-steped.png")
         self.one_shot = False
         self.consumed = False
+        self.victims = randint(1,4)
 #        self.texture = texture # TODO: define
     
     def trigger(self):
-        self.game.states[gameState.Running].player.in_shelter = True
-        self.game.states[gameState.Running].player.steps = self.game.states[gameState.Running].steps_max
-        self.game.states[gameState.Running].hud.textBox.text = "You entered a shelter."
-        self.game.states[gameState.Running].world.shader_modifiers.append(+8)
-        self.game.states[gameState.Running].player.days += 1
-        self.game.states[gameState.Running].hud.dayDisplay.switch_icon()
+        game = self.game.states[gameState.Running]
+        game.player.in_shelter = True
+        game.player.hide()
+        game.player.steps = self.game.states[gameState.Running].steps_max
+        game.hud.textBox.text = "You entered a shelter."
+        game.world.shader_modifiers.append(+8)
+        game.player.days += 1
+        game.hud.dayDisplay.switch_icon()
+        if game.player.days % 4 == 0: # every 4 days
+            game.player.exhaust()
+            game.hud.textBox.text += "You where exhausted and your blood level sank."
         
         if not self.consumed:
             self.consumed = True
-            self.game.states[gameState.Running].player.bloodPoints += 1
-            self.game.states[gameState.Running].hud.textBox.text += " Lucky you.. you found some innocent victims."
-        # TODO: remove alpha blending
-        # TODO: make player invisible
-        # TODO: display notification of shelter
-        # TODO: increase the bloodpoints
+            game.player.feed(self.victims)
+            game.hud.textBox.text += " Lucky you.. you found %s innocent victims." % self.victims
+            self.texture = self.texture_consumed
     
     def draw(self):
         oldTexture = Texture.currentTexture
